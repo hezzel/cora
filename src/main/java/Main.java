5c06568149e590dbd25603c5d0559c35dc2e5df0
Cynthia Kop <kop@science.ru.nl>
2019-05-26 19:56:47 +0200

    Building a lexer for the default Cora grammar.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ package cora;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("This is Cora.");
+    cora.parsers.LexerTester.printTokens("inputshape.txt");
   }
 }
