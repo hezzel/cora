d20f4694509aa769f7a7ff36ae42dea112f311f8
Cynthia Kop <kop@science.ru.nl>
2019-08-09 16:47:45 +0200

    Introduced data type for rules.

---
+++ src/main/java/interfaces/rewriting/Rule.java
@@ -0,0 +1,41 @@
+/**************************************************************************************************
+ Copyright 2019 Cynthia Kop
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ *************************************************************************************************/
+
+package cora.interfaces.rewriting;
+
+import java.util.ArrayList;
+import cora.interfaces.types.Type;
+import cora.interfaces.terms.Term;
+
+/**
+ * Rules are the core objects that define the reduction relation in a term rewriting system.
+ * They have the form l -> r, where l and r have the same type.
+ *
+ * Note; all instances of Rule must (and can be expected to) be immutable.
+ */
+public interface Rule {
+  /** For a rule l -> r, this function returns l. */
+  Term queryLeftSide();
+
+  /** For a rule l -> r, this function returns r. */
+  Term queryRightSide();
+
+  /** For a rule l -> r, returns the type of l (which should also be the type of r). */
+  Type queryType();
+
+  /** Gives a string representation of the current rule. */
+  String toString();
+}
+
---
+++ src/test/java/RuleTest.java
@@ -0,0 +1,75 @@
+/**************************************************************************************************
+ Copyright 2018 Cynthia Kop
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ *************************************************************************************************/
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import cora.exceptions.NullInitialisationError;
+import cora.exceptions.TypingError;
+import cora.interfaces.types.Type;
+import cora.interfaces.terms.Term;
+import cora.interfaces.rewriting.Rule;
+import cora.immutabledata.types.*;
+import cora.immutabledata.terms.*;
+import cora.immutabledata.rewriting.SimpleRule;
+
+public class RuleTest {
+  private Type baseType(String name) {
+    return new Sort(name);
+  }
+
+  private Type arrowType(String name1, String name2) {
+    return new ArrowType(baseType(name1), baseType(name2));
+  }
+
+  private Term constantTerm(String name, Type type) {
+    return new FunctionalTerm(new UserDefinedSymbol(name, type));
+  }
+
+  private Term unaryTerm(String name, Type output, Term arg) {
+    Type arrtype = new ArrowType(arg.queryType(), output);
+    UserDefinedSymbol f = new UserDefinedSymbol(name, arrtype);
+    return new FunctionalTerm(f, arg);
+  }
+
+  @Test(expected = NullInitialisationError.class)
+  public void leftNullCreation() {
+    Rule rule = new SimpleRule(constantTerm("a", baseType("b")), null);
+  }
+
+  @Test(expected = NullInitialisationError.class)
+  public void rightNullCreation() {
+    Rule rule = new SimpleRule(null, constantTerm("a", baseType("b")));
+  }
+
+  @Test(expected = TypingError.class)
+  public void mistypedRule() {
+    Var x = new Var("x", baseType("a"));
+    Term left = unaryTerm("id", baseType("b"), x);
+    Rule rule = new SimpleRule(left, x);
+  }
+
+  @Test
+  public void testBasics() {
+    Var x = new Var("x", baseType("a"));
+    Term left = unaryTerm("id", baseType("a"), x);
+    Rule rule = new SimpleRule(left, x);
+    assertTrue(rule.queryLeftSide().equals(left));
+    assertTrue(rule.queryRightSide().equals(x));
+    assertTrue(rule.queryType().equals(baseType("a")));
+    assertTrue(rule.toString().equals("id(x) → x"));
+  }
+}
+
