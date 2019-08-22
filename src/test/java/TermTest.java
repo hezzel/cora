566a1ef7d083be2c25d9013f8c4c5d7e186a559b
Cynthia Kop <kop@science.ru.nl>
2019-08-22 23:30:17 +0200

    Replaced 2018 by 2019 in copyright (oops).

--- src/test/java/TermTest.java
+++ src/test/java/TermTest.java
@@ -1,5 +1,5 @@
 /**************************************************************************************************
- Copyright 2018 Cynthia Kop
+ Copyright 2019 Cynthia Kop
 
  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  in compliance with the License.
