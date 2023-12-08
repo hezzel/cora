08e98d4f053279a723f27250289e964a82d1137d
Cynthia Kop <kop@science.ru.nl>
2023-12-08 13:44:25 +0100

    Added support for div and mod (but not in the parser yet).

---
+++ app/src/main/java/cora/terms/DivModSymbol.java
@@ -0,0 +1,61 @@
+/**************************************************************************************************
+ Copyright 2023 Cynthia Kop
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ *************************************************************************************************/
+
+package cora.terms;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import cora.types.TypeFactory;
+
+/** The binary division or modulo function. */
+class DivModSymbol extends CalculationInherit {
+  private final boolean _division; // true if we are ∨, false if we are ∧
+
+  DivModSymbol(boolean isDiv) {
+    // Int ⇒ Int ⇒ Int
+    super(TypeFactory.createArrow(TypeFactory.intSort, TypeFactory.createArrow(
+      TypeFactory.intSort, TypeFactory.intSort)));
+    _division = isDiv;
+  }
+
+  public String queryName() {
+    if (_division) return "/";
+    else return "%";
+  }
+
+  public int queryArity() {
+    return 2;
+  }
+
+  public String toUniqueString() {
+    if (_division) return "/";
+    else return "%";
+  }
+
+  public int queryInfixPriority() {
+    return CalculationSymbol.INFIX_DIVMOD;
+  }
+
+  public boolean printInfix(StringBuilder builder, List<Term> args,
+                            Map<Replaceable,String> renaming, Set<String> avoid) {
+    if (args.size() != 2) return false; // let the standard printing procedure handle it
+    // no associativity!
+    printHelper(builder, args.get(0), renaming, avoid, CalculationSymbol.INFIX_DIVMOD);
+    builder.append(" " + queryName() + " ");
+    printHelper(builder, args.get(1), renaming, avoid, CalculationSymbol.INFIX_DIVMOD);
+    return true;
+  }
+}
