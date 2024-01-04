0d2947fa90365aa54598db15210aa729d0b157a6
Deivid Vale <deividrodriguesvale@gmail.com>
2024-01-05 00:52:19 +0100

    Artifact production for esop.

---
+++ cora_distribution/run_exp_all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+EXP_PATH="./benchmarks"
+
+FILE_COUNTER=0
+
+# Run paper experiments
+echo "Running paper experiments..."
+
+FILES="$EXP_PATH/paper_examples/*"
+
+for f in $FILES
+do
+    echo "Paper experiment $FILE_COUNTER..."
+    ./bin/app "$f"
+    echo "Done."
+    FILE_COUNTER=$((FILE_COUNTER+1))
+done
+FILE_COUNTER=0
+
+rm -rf result
+rm -rf problem.smt2
+
+# Run extra experiments
+FILES="$EXP_PATH/extra_examples/*"
+for f in $FILES
+do
+    echo "Extra experiment $FILE_COUNTER..."
+    ./bin/app "$f"
+    echo "Done."
+    FILE_COUNTER=$((FILE_COUNTER+1))
+done
+
+rm -rf result
+rm -rf problem.smt2
