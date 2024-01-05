f3b1c6650826c36af2d5ce9bdf429866041e349a
Cynthia Kop <kop@science.ru.nl>
2024-01-05 01:01:57 +0100

    Moved over a test to Jupiter

--- app/src/test/java/cora/smt/SmtSolverTest.java
+++ app/src/test/java/cora/smt/SmtSolverTest.java
@@ -15,8 +15,10 @@
 
 package cora.smt;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.TreeSet;
 
 public class SmtSolverTest {
