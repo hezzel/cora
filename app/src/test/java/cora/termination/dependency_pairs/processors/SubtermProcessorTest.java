ef5512935e0fc9e218498c0db669c5bb278a1b5a
Deivid Vale <deividrodriguesvale@gmail.com>
2024-02-07 23:49:24 +0100

    Adds basic testing for subterm processor.

--- app/src/test/java/cora/termination/dependency_pairs/processors/SubtermProcessorTest.java
+++ app/src/test/java/cora/termination/dependency_pairs/processors/SubtermProcessorTest.java
@@ -1,9 +1,35 @@
 package cora.termination.dependency_pairs.processors;
 
+import cora.reader.CoraInputReader;
+import cora.rewriting.TRS;
+import cora.termination.dependency_pairs.DPGenerator;
+import cora.termination.dependency_pairs.Problem;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubtermProcessorTest {
 
+  @Test
+  void testProcessDPP() {
+    // This is the TRS that fails and shouldn't
+    TRS trs = CoraInputReader.readTrsFromString(
+      "sum :: Int -> Int \n" +
+        "sum(x) -> sum(x - 1) | x > 0"
+    );
 
+    Problem p = DPGenerator.generateProblemFromTrs(trs);
 
+    // Let's get SCCS for it...
+    GraphProcessor graphProc = new GraphProcessor();
+    List<Problem> sccProblems = graphProc.processDPP(p).get();
+    Problem pToTest = sccProblems.getFirst();
+
+    SubtermProcessor subProc = new SubtermProcessor();
+
+    subProc.processDPP(pToTest);
+
+  }
 }
