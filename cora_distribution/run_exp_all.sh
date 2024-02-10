600882cf64ee0a383e9d8e9b1013213ac535e00a
Deivid Vale <deividrodriguesvale@gmail.com>
2024-02-10 13:55:15 +0100

    Updates running script

--- cora_distribution/run_exp_all.sh
+++ cora_distribution/run_exp_all.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Terminal colors
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
 EXP_PATH="./benchmarks"
 
 FILE_COUNTER=0
@@ -6,25 +13,28 @@ FILE_COUNTER=0
 # Run paper experiments
 echo "Running paper experiments..."
 
+echo "Running experiments from ESOP2024..."
 FILES="$EXP_PATH/esop2024/*"
 
 for f in $FILES
 do
-    echo "Paper experiment $FILE_COUNTER..."
+    printf "${PURPLE}Experiment number $FILE_COUNTER...${NC}\n"
+    printf "${BLUE}Invoking cora on file $f ${NC}"
     ./bin/app "$f"
-    echo "Done."
+    printf "Done."
     FILE_COUNTER=$((FILE_COUNTER+1))
 done
-FILE_COUNTER=0
 
 rm -rf result
 rm -rf problem.smt2
 
 # Run extra experiments
+echo "Running experiments from IJCAR2024..."
 FILES="$EXP_PATH/ijcar2024/*"
 for f in $FILES
 do
-    echo "Extra experiment $FILE_COUNTER..."
+    printf "${PURPLE}Experiment number $FILE_COUNTER...${NC}\n"
+    printf "${BLUE}Invoking cora on file $f ${NC}"
     ./bin/app "$f"
     echo "Done."
     FILE_COUNTER=$((FILE_COUNTER+1))
@@ -33,11 +43,12 @@ done
 rm -rf result
 rm -rf problem.smt2
 
-# Run extra experiments
+echo "Running experiments from TPDB_ITRS..."
 FILES="$EXP_PATH/tpdb_itrs/*"
 for f in $FILES
 do
-    echo "Extra experiment $FILE_COUNTER..."
+    printf "${PURPLE}Experiment number $FILE_COUNTER...${NC}\n"
+    printf "${BLUE}Invoking cora on file $f ${NC}"
     ./bin/app "$f"
     echo "Done."
     FILE_COUNTER=$((FILE_COUNTER+1))
@@ -46,11 +57,12 @@ done
 rm -rf result
 rm -rf problem.smt2
 
-# Run extra experiments
+echo "Running extra experiments..."
 FILES="$EXP_PATH/extra/*"
 for f in $FILES
 do
-    echo "Extra experiment $FILE_COUNTER..."
+    printf "${PURPLE}Experiment number $FILE_COUNTER...${NC}\n"
+    printf "${BLUE}Invoking cora on file $f ${NC}"
     ./bin/app "$f"
     echo "Done."
     FILE_COUNTER=$((FILE_COUNTER+1))
