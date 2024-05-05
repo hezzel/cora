c909bdb89c77e76e735187e9d60ee9b58a391915
Cynthia Kop <kop@science.ru.nl>
2024-05-05 19:10:00 +0200

    Remove the instancing from config.Settings

--- app/src/main/java/cora/config/Settings.java
+++ app/src/main/java/cora/config/Settings.java
@@ -17,7 +17,6 @@ package cora.config;
 
 import charlie.smt.SmtSolver;
 import charlie.solvesmt.ProcessSmtSolver;
-import charlie.solvesmt.ProcessSmtSolver.*;
 import java.util.Set;
 
 /**
@@ -26,47 +25,21 @@ import java.util.Set;
  * settings that are not set), and can be queried from any class outside of the cora library.
  */
 public class Settings {
-
-  // Sets cora to use the external smt solver.
-  // This one writes the smtstring as a file on disk and reads the result back from it.
-  // It requires auxiliary files in order to run cora on it.
-  // Before using this solver, please make sure such script is present on disk.
-  // public static SmtSolver smtSolver = new ExternalSmtSolver();
-
-  private static PhysicalSolver physicalSolver =
-    PhysicalSolver.Z3;
-
-  public static SmtSolver smtSolver =
-    new ProcessSmtSolver(physicalSolver);
-
+  public static SmtSolver smtSolver = new ProcessSmtSolver(ProcessSmtSolver.PhysicalSolver.Z3);
   public static Set<String> disabled = Set.of();
 
+  /** Use this to check if a technique is diabled (by name). */
   public static boolean isDisabled(String technique) {
     return disabled.contains(technique);
   }
 
-  private static Settings settingsInstance;
-
-  private Settings() {}
-
-  /**
-   * Lazily Returns the unique instance of this singleton object.
-   * If the settings isn't instantiated yet,
-   * this method create such instance before returning it.
-   * Up to now the default settings is:
-   * SmtSolver to be used: ProcessSolver
-   * PhysicalSolver to be used: Z3.
-   * @return
-   */
-  public static Settings getSettingsInstance() {
-    if (settingsInstance == null) {
-      settingsInstance = new Settings();
-    }
-    return settingsInstance;
+  /** Used to set up the SMT solver. */
+  public static void setSolver(SmtSolver solver) {
+    smtSolver = solver;
   }
 
-  public static void setPhysicalSolver(PhysicalSolver physicalSolver) {
-    Settings.physicalSolver = physicalSolver;
+  /** Used to set up which techniques are disabled. */
+  public static void setDisabled(Set<String> disabledTechniques) {
+    disabled = disabledTechniques;
   }
-
 }
