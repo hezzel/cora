bf3689688bde903d8a6e9734d49501d02549f628
Cynthia Kop <kop@science.ru.nl>
2024-11-16 23:53:25 +0100

    Added forgotten AriInputReader files to git

---
+++ app/src/main/java/charlie/reader/AriInputReader.java
@@ -0,0 +1,225 @@
+/**************************************************************************************************
+ Copyright 2023--2024 Cynthia Kop
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ *************************************************************************************************/
+
+package charlie.reader;
+
+import com.google.common.collect.ImmutableList;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import charlie.exceptions.*;
+import charlie.util.LookupMap;
+import charlie.types.*;
+import charlie.parser.lib.Token;
+import charlie.parser.lib.ErrorCollector;
+import charlie.parser.Parser;
+import charlie.parser.Parser.*;
+import charlie.parser.AriParser;
+import charlie.terms.*;
+import charlie.trs.*;
+
+/**
+ * This class reads text from string or file written in the .ari format used for the international
+ * termination and confluence competitions.  For now, this is limited to the higher-order format.
+ * In the future it may be expanded to first-order and constrained formats (which are also included
+ * in ARI).
+ */
+public class AriInputReader extends TermTyper {
+  /**
+   * Stores the error collector for use by methods of the AriInputReader class.
+   * Private because it should only be called by the static methods that use a AriInputReader.
+   */
+  private AriInputReader(ErrorCollector collector) {
+    super(new SymbolData(), collector);
+  }
+
+  // ===================================== TRANSLATING A RULE =====================================
+
+  /**
+   * If the given identifier represents a free variable (that is, it does not occur in bound, and
+   * is not declared as a function symbol), then this function stores numberArguments as its
+   * arity.  If this is inconsistent with a previously declared arity for this variable, then an
+   * error is stored instead.
+   */
+  private void registerVariableArity(Token token, String name, int numberArguments,
+                                     TreeMap<String,Integer> arity, TreeSet<String> bound) {
+    if (_symbols.lookupFunctionSymbol(name) != null) return;
+    if (bound.contains(name)) return;
+    if (arity.containsKey(name)) {
+      if (arity.get(name) != 0) {
+        storeError("Inconsistent arity of variable " + name + ": occurs with no arguments " +
+          "here, while it previously occurred with " + arity.get(name) + ".", token);
+        return;
+      }
+    }
+    arity.put(name, numberArguments);
+  }
+
+  /**
+   * This goes through the given parser term, and for each unbound variable, it stores the number of
+   * arguments with which it occurs.  By definition of the format, this arity should be consistent
+   * in the full term.
+   */
+  private void storeVariableArity(ParserTerm term, TreeMap<String,Integer> arity,
+                                  TreeSet<String> bound, int numArgs) {
+    switch (term) {
+      case Identifier(Token token, String name):
+        registerVariableArity(token, name, numArgs, arity, bound);
+        break;
+      case Lambda(Token token, String varname, Type type, ParserTerm arg):
+        if (bound.contains(varname)) storeVariableArity(arg, arity, bound, 0);
+        else {
+          bound.add(varname);
+          storeVariableArity(arg, arity, bound, 0);
+          bound.remove(varname);
+        }
+        break;
+      case Application(Token token, ParserTerm head, List<ParserTerm> args):
+        storeVariableArity(head, arity, bound, args.size());
+        for (ParserTerm t : args) storeVariableArity(t, arity, bound, 0);
+        break;
+      case PErr e:
+        break;
+      default:
+        storeError("Unexpected term shape in ARI unconstrained higher-order format.", term.token());
+    }
+  }
+
+  /**
+   * This function returns an alteration of term where variable applications X(s1,...,sn) are
+   * replaced by the meta-application X[s1,...,sn] if arity[X] = n.
+   */
+  private ParserTerm replaceMetaVariables(ParserTerm term, TreeMap<String,Integer> arity) {
+    return switch (term) {
+      case Identifier id -> id;
+      case Lambda(Token token, String varname, Type type, ParserTerm arg) -> {
+          Integer backup = arity.get(varname);
+          if (backup != null) arity.remove(varname);
+          ParserTerm ret = new Lambda(token, varname, type, replaceMetaVariables(arg, arity));
+          if (backup != null) arity.put(varname, backup);
+          yield ret;
+        }
+      case Application(Token token, ParserTerm head, List<ParserTerm> args) -> {
+          ImmutableList.Builder<ParserTerm> newargs = ImmutableList.<ParserTerm>builder();
+          for (ParserTerm arg : args) newargs.add(replaceMetaVariables(arg, arity));
+          if (head instanceof Identifier(Token t, String name) &&
+              arity.containsKey(name) && arity.get(name) > 0) {
+            yield new Meta(token, name, newargs.build());
+          }
+          else yield new Application(token, replaceMetaVariables(head, arity), newargs.build());
+        }
+      case PErr e -> term;
+      default -> {
+          storeError("Unexpected term shape.", term.token());
+          yield new PErr(term);
+        }
+    };
+  }
+
+  private Rule makeRule(ParserRule rule) {
+    TreeMap<String,Integer> variableArity = new TreeMap<String,Integer>();
+    TreeSet<String> bound = new TreeSet<String>();
+    storeVariableArity(rule.left(), variableArity, bound, 0);
+    ParserTerm left = replaceMetaVariables(rule.left(), variableArity);
+    ParserTerm right = replaceMetaVariables(rule.right(), variableArity); 
+    _symbols.clearEnvironment();
+    Term l = null, r = null;
+    int a = _errors.queryErrorCount();
+    if (!left.hasErrors()) l = makeTerm(left, null, true);
+    if (!right.hasErrors()) {
+      r = makeTerm(right, l == null ? null : l.queryType(), false);
+    }
+    int b = _errors.queryErrorCount();
+    if (l == null || r == null) return null;
+    try { return TrsFactory.createRule(l, r, TrsFactory.AMS); }
+    catch (IllegalRuleException e) {
+      if (a == b) _errors.addError(e.getMessage());
+      return null;
+    }
+  }
+
+  // =================================== READING A FULL PROGRAM ===================================
+
+  private void makeAlphabet(LookupMap<ParserDeclaration> fundecs) {
+    for (ParserDeclaration decl : fundecs.values()) {
+      String name = decl.name();
+      Type type = decl.type();
+      if (_symbols.lookupFunctionSymbol(name) != null) {
+        storeError("Duplicate function symbol: " + name, decl.token());
+      }
+      else _symbols.addFunctionSymbol(TermFactory.createConstant(name, type));
+    }
+  }
+
+  /** This function turns the entire ParserProgram into a TRS. */
+  private TRS makeTRS(ParserProgram trs) {
+    makeAlphabet(trs.fundecs());
+    ArrayList<Rule> rules = new ArrayList<Rule>();
+    for (ParserRule rho : trs.rules()) {
+      Rule r = makeRule(rho);
+      if (r != null) rules.add(r);
+    }   
+    Alphabet alphabet = _symbols.queryCurrentAlphabet();
+    try { return TrsFactory.createTrs(alphabet, rules, TrsFactory.AMS); }
+    catch (IllegalRuleException e) {
+      _errors.addError(e.getMessage());
+      return null;
+    }
+  }
+
+  // ==================================== PUBLIC FUNCTIONALITY ====================================
+
+  /** Throws a ParseException if there are any errors stored in the given error collector */
+  private static void throwIfAnyErrors(ErrorCollector collector) {
+    if (collector.queryErrorCount() > 0) {
+      throw new ParseException(collector.queryCollectedMessages());
+    }
+  }
+
+  /** Helper function for readTrsFromString and readTrsFromFile */
+  private static TRS readParserProgram(ParserProgram trs, ErrorCollector collector) {
+    throwIfAnyErrors(collector);
+    AriInputReader rd = new AriInputReader(collector);
+    TRS ret = rd.makeTRS(trs);
+    throwIfAnyErrors(collector);
+    return ret;
+  }
+
+  /**
+   * Parses the given program, and returns the integer TRS that it defines.
+   * If the string is not correctly formed, or the system cannot be unambiguously typed as an
+   * LCTRS, this may throw a ParseException.
+   */
+  public static TRS readTrsFromString(String str) {
+    ErrorCollector collector = new ErrorCollector();
+    ParserProgram trs = AriParser.readProgramFromString(str, collector);
+    return readParserProgram(trs, collector);
+  }
+
+  /**
+   * Parses the given file, which should be a .itrs file, into an LCTRS.
+   * This may throw a ParseException, or an IOException if something goes wrong with the file
+   * reading.
+   */
+  public static TRS readTrsFromFile(String filename) throws IOException {
+    ErrorCollector collector = new ErrorCollector();
+    ParserProgram trs = AriParser.readProgramFromFile(filename, collector);
+    return readParserProgram(trs, collector);
+  }
+}
---
+++ app/src/test/java/charlie/reader/AriInputReaderTest.java
@@ -0,0 +1,143 @@
+/**************************************************************************************************
+ Copyright 2019--2024 Cynthia Kop
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ *************************************************************************************************/
+
+package charlie.reader;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import charlie.exceptions.ParseException;
+import charlie.trs.TRS;
+
+public class AriInputReaderTest {
+  @Test
+  public void testReadAlphabet() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort a)\n" +
+      "(sort list)\n" +
+      "(fun cons (-> a list list))\n" +
+      "(fun map (-> list (-> a a) list))\n" +
+      "(fun nil list)\n" +
+      "(fun extra (-> a (-> (-> a a) a) a))\n";
+    TRS trs = AriInputReader.readTrsFromString(str);
+    assertTrue(trs.queryAlphabet().getSymbols().size() == 4);
+    assertTrue(trs.queryAlphabet().lookup("nil").queryType().toString().equals("list"));
+    assertTrue(trs.queryAlphabet().lookup("cons").queryType().toString().equals("a → list → list"));
+    assertTrue(trs.queryAlphabet().lookup("map").queryType().toString().equals("list → (a → a) → list"));
+    assertTrue(trs.queryAlphabet().lookup("extra").queryType().toString().equals("a → ((a → a) → a) → a"));
+  }
+
+  @Test
+  public void testReadRuleWithoutMeta() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort a)\n" +
+      "(sort list)\n" +
+      "(fun cons (-> a list list))\n" +
+      "(fun map (-> list (-> a a) list))\n" +
+      "(fun nil list)\n" +
+      "(fun start (-> list list))\n" +
+      "(rule (map nil F) nil)\n" +
+      "(rule (map (cons h t) F) (cons (F h) (map t F)))\n" +
+      "(rule (start lst) (map lst (lambda ((x a)) x)))\n";
+    TRS trs = AriInputReader.readTrsFromString(str);
+    assertTrue(trs.queryRuleCount() == 3);
+    assertTrue(trs.queryRule(0).toString().equals("map(nil, F) → nil"));
+    assertTrue(trs.queryRule(1).toString().equals("map(cons(h, t), F) → cons(F(h), map(t, F))"));
+    assertTrue(trs.queryRule(2).toString().equals("start(lst) → map(lst, λx.x)"));
+  }
+
+  @Test
+  public void testReadRuleWithMeta() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort real)\n" +
+      "(fun der (-> (-> real real) real real))\n" +
+      "(fun plus (-> real real real))\n" +
+      "(rule (der (lambda ((x real)) (plus (F x) (G x))))\n" +
+      "      (lambda ((x real)) (plus (der (lambda ((y real)) (F y)) x)\n" +
+                                     "(der (lambda ((y real)) (G y)) x))))\n";
+    TRS trs = AriInputReader.readTrsFromString(str);
+    assertTrue(trs.queryRuleCount() == 1);
+    assertTrue(trs.queryRule(0).toString().equals(
+      "der(λx.plus(F⟨x⟩, G⟨x⟩)) → λx.plus(der(λy.F⟨y⟩, x), der(λy.G⟨y⟩, x))"));
+  }
+
+  @Test
+  public void testLambdaWithHigherOrderBinder() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort a)\n" +
+      "(sort b)\n" +
+      "(fun B b)\n" +
+      "(fun f (-> (-> b (-> a b) b) a))\n" +
+      "(rule (f (lambda ((y b) (x (-> a b))) (x (Z y)))) (Z B))\n";
+    TRS trs = AriInputReader.readTrsFromString(str);
+    assertTrue(trs.queryRuleCount() == 1);
+    assertTrue(trs.queryRule(0).toString().equals("f(λy.λx.x(Z⟨y⟩)) → Z⟨B⟩"));
+  }
+
+  @Test
+  public void testDuplicateBinderName() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort a)\n" +
+      "(sort b)\n" +
+      "(fun f (-> (-> b b) a a))\n" +
+      "(fun g (-> (-> a a) a))\n" +
+      "(fun A a)\n" +
+      "(rule (g (lambda ((x a)) (f (lambda ((x b)) x) (Z x)))) (Z A))\n";
+    TRS trs = AriInputReader.readTrsFromString(str);
+    assertTrue(trs.queryRuleCount() == 1);
+    assertTrue(trs.queryRule(0).toString().equals("g(λx.f(λx1.x1, Z⟨x⟩)) → Z⟨A⟩"));
+  }
+
+  @Test
+  public void testBinderOverlapsMetaVariableName() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort o)\n" +
+      "(fun I o)\n" +
+      "(fun f (-> (-> (-> o o) o) (-> o o o) o o))\n" +
+      "(rule (f (lambda ((v (-> o o))) (v I)) (lambda ((y o) (z o)) (v y z)) x) (v x I))\n";
+    TRS trs = AriInputReader.readTrsFromString(str);
+    assertTrue(trs.queryRuleCount() == 1);
+    assertTrue(trs.queryRule(0).toString().equals("f(λv1.v1(I), λy.λz.v⟨y, z⟩, x) → v⟨x, I⟩"));
+  }
+
+  @Test
+  public void testInconsistentArity() {
+    String str =
+      "(format higher-order)\n" +
+      "(sort o)\n" +
+      "(fun I o)\n" +
+      "(fun f (-> (-> (-> o o) o) (-> o o o) o o))\n" +
+      "(fun g (-> (-> o o) o o))\n" +
+      "(rule (f (lambda ((v (-> o o))) (v I)) (lambda ((y o) (z o)) (v y z)) x) (v x))\n" +
+      "(rule (g (lambda ((x o)) (Z x)) Z) I)\n";
+    try { AriInputReader.readTrsFromString(str); }
+    catch ( ParseException e ) {
+      assertTrue(e.getMessage().equals(
+        "6:75: Meta-variable v was previously used (or declared) with arity 2, but is here " +
+          "used with 1 arguments.\n" +
+        "7:33: Inconsistent arity of variable Z: occurs with no arguments here, while it " +
+          "previously occurred with 1.\n"));
+      return;
+    }
+    assertTrue(false);
+  }
+}
+
