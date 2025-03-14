4e5964ee5fcda0c972745dfdb386ffcda3dff7f9
Liye Guo <guo.ly@hotmail.com>
2025-03-14 16:48:01 +0100

    Add methods to package charlie.trs for the critical peak finder.

--- app/src/test/java/charlie/trs/TrsTest.java
+++ app/src/test/java/charlie/trs/TrsTest.java
@@ -23,12 +23,15 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
+import charlie.types.Arrow;
+import charlie.types.Base;
 import charlie.types.Type;
 import charlie.types.TypeFactory;
 import charlie.parser.CoraParser;
 import charlie.terms.*;
 import charlie.trs.TrsProperties.*;
 import charlie.trs.TRS.RuleScheme;
+import charlie.reader.CoraInputReader;
 
 public class TrsTest {
   private Type type(String txt) {
@@ -300,6 +303,50 @@ public class TrsTest {
     rules.add(TrsFactory.createRule(left, a));  // g(y+y) -> A
     trs = TrsFactory.createTrs(alf, rules, TrsFactory.LCSTRS);
     assertFalse(trs.isLeftLinear());
+  }
+
+  @Test
+  public void testQueryRulesForSymbolFact() {
+    var trs = CoraInputReader.readTrsFromString(
+      "fact :: Int -> (Int -> o) -> o\n" +
+      "comp :: (Int -> o) -> (Int -> Int) -> Int -> o\n" +
+      "fact(n, k) -> k(1) | n <= 0\n" +
+      "fact(n, k) -> fact(n - 1, comp(k, [*](n))) | n > 0\n" +
+      "comp(g, f, x) -> g(f(x))\n");
+    var fact = makeConstant("fact", "Int -> (Int -> o) -> o");
+    var factRules = trs.queryRulesForSymbol(fact, false).toList();
+    assertEquals(2, factRules.size());
+    assertTrue(factRules.stream().allMatch(r -> fact.equals(r.queryRoot())));
+
+    var comp = makeConstant("comp", "(Int -> o) -> (Int -> Int) -> Int -> o");
+    var compRules = trs.queryRulesForSymbol(comp, false).toList();
+    assertEquals(1, compRules.size());
+    assertTrue(compRules.stream().allMatch(r -> comp.equals(r.queryRoot())));
+
+    var udef = makeConstant("udef", "o");
+    assertEquals(0, trs.queryRulesForSymbol(udef, false).count());
+  }
+
+  @Test
+  public void testQueryRulesForSymbolVarHead() {
+    var trs = CoraInputReader.readTrsFromString(
+      "{ F :: a -> a } F(x) -> x\n" +
+      "{ G :: b -> a -> a } G(x, y) -> y\n");
+    var f = makeConstant("f", "a -> a");
+    var fVarRules = trs.queryRulesForSymbol(f, true).toList();
+    assertEquals(1, fVarRules.size());
+    assertTrue(fVarRules.stream().allMatch(
+      r -> r.queryLeftSide().queryHead().queryType().equals(
+        new Arrow(new Base("a"), new Base("a")))));
+
+    var fg = makeConstant("fg", "c -> b -> a -> a");
+    assertEquals(2, trs.queryRulesForSymbol(fg, true).count());
+
+    var h = makeConstant("h", "b -> a -> b");
+    assertEquals(0, trs.queryRulesForSymbol(h, true).count());
+
+    var i = makeConstant("i", "a");
+    assertEquals(0, trs.queryRulesForSymbol(i, true).count());
   }
 }
 
