d7c7317fb1ec3a4914e8f0a8458f08c4a5731d4c
Cynthia Kop <kop@science.ru.nl>
2025-12-10 00:17:06 +0100

    Removed the forced versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,13 +19,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
 application {
     // Define the main class for the application.
     mainClass.set("cora.App")
